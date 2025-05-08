b31bcbf08ccc59210231245d32ea348686eb3b82
Saico11 <enrique.saico@ucsm.edu.pe>
2025-05-08 15:49:09 -0500

    fix(error archivos):Elimina archivos de error al mover de carpetas y hacer commits desde consola

--- PracticaN_6/Ejercicios/ExceptionIsEmpty.java
+++
@@ -1,7 +0,0 @@
-// Excepcion personalizada para operaciones invalidas en pilas vacias
-public class ExceptionIsEmpty extends Exception {
-    public ExceptionIsEmpty(String message) {
-        super(message);
-    }
-}
-
